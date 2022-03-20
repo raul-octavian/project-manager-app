2b114a8109ca80bcc83f50fc49f5ec35402d31b6
raul-octavian <alburaul@gmail.com>
2022-03-20 08:18:37 +0100

    heroku deployment

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ require('dotenv-flow').config();
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE"); // If using .fetch and not axios
-  res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE"); // If using .fetch and not axios
+//   res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// })
 
 app.use(express.json());
 
